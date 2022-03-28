66501084adf3ec6e18754635a1fc89b4b73c48ab
ventusboy <42723217+ventusboy@users.noreply.github.com>
2022-03-27 21:28:07 -0400

    mobile cards have been fixed

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,7 +5,6 @@ import { useAuth0 } from "../react-auth0-spa";
 
 const Profile = () => {
   const { loading, user, logout, isAuthenticated } = useAuth0();
-  console.log('yikes');
 
   if (loading) {
     return <div>Loading...</div>;
@@ -14,16 +13,13 @@ const Profile = () => {
   }
 
   return (
-
-    <Fragment>
-      {console.log(user)}
-      <img src={user.picture} alt="Profile" style={{ height: "100px" }} />
-
+    <div className="d-flex flex-column align-content-center">
+      <img src={user.picture} alt="Profile" style={{ height: "100px", width: "100px"}} referrerPolicy="no-referrer" />
       <h2>{user.name}</h2>
       <p>{user.email}</p>
       <code>{JSON.stringify(user, null, 2)}</code>
       {isAuthenticated && <button className='btn btn-outline-primary justify-self-end' onClick={() => logout({ returnTo: window.location.origin })}>Log out</button>}
-    </Fragment>
+    </div>
   );
 };
 
--- src/components/Singlecard.js
+++ src/components/Singlecard.js
@@ -10,16 +10,20 @@ function Singlecard (props) {
     return (
         <div id={item.area + '-' + item.code} className={preReqsMet(item) + ' classCard col-10 container offset-1'}>
             <div className="row">
-                <h3 className="col-xl-4 col-sm-4">{item.area + ' ' + item.code}</h3>
-                <h3 className="col-xl-7 col-sm-6">{item.name}</h3>
-                <img className="col-xl-1 col-sm-2 deletebtn d-flex justify-content-end" src="./assets/delete.svg" onClick={() => { onDelete() }}  alt="Delete Icon"  />
+                <div className="col-10 d-flex flex-column">
+                    <h3 className="col-xl-4 col-sm-4">{item.area + ' ' + item.code}</h3>
+                    <h4 className="col-xl-7 col-sm-6 text-capitalize">{item.name}</h4>
+                </div>
+                <div className="col-2 col-xl-1 col-sm-2 d-flex justify-content-end">
+                    <img className="deletebtn" src="./assets/delete.svg" onClick={() => { onDelete() }}  alt="Delete Icon"  />
+                </div>
             </div>
             <div className="row">
-                <h4 className="col-8">pre-reqs: <PreReqList list={item.preReqs} /> </h4>
-                <h4 className="col-4">completed: {item.completed ? 'yes' : 'no'}</h4>
+                <p className="col-12 text-capitalize">Description: the course where you learn all about {item.name}</p>
             </div>
             <div className="row">
-                <p className="col-12">description: the course where you learn all about {item.name}</p>
+                <h5 className="col-12 col-md-8">Pre-Reqs: <PreReqList list={item.preReqs} /> </h5>
+                <h6 className="col-12 col-md-4">Completed: {item.completed ? 'Yes' : 'No'}</h6>
             </div>
         </div>
     );
@@ -36,6 +40,7 @@ function PreReqList(props) {
             onClick={navigateTo}
             className="btn btn-secondary"
             value={`${item.area}-${item.code}`} 
+            style={{ margin: "2px 3px"}}
         >
             {`${item.area}-${item.code}`} 
         </button>
@@ -46,6 +51,7 @@ function navigateTo(obj) {
     let button = obj.target;
     let elmnt = document.getElementById(button.value);
     elmnt && elmnt.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+    button.blur()
 }
 
 function preReqsMet(item) {
